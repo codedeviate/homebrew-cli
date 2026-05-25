151f01163a9dd3ecee44529dc7e173c5cda7dd4b
Thomas Björk <codedv8@gmail.com>
2026-05-25 21:47:58 +0200

    batty 0.13.0 — fix sha256

--- Formula/batty.rb
+++ Formula/batty.rb
@@ -2,7 +2,7 @@ class Batty < Formula
   desc "Cat clone with syntax highlighting, git integration, and Rhai support"
   homepage "https://github.com/codedeviate/batty"
   url "https://github.com/codedeviate/batty/archive/refs/tags/v0.13.0.tar.gz"
-  sha256 "8bc17473a7bf9a2748137094ba8a9c79cf7cfa556d2c8b59796e21f77c98ac3e"
+  sha256 "8f8e8a7ad3ae84dadd30b14a69787dfaa6cc707e516fe59db3548753da185656"
   license "MIT"
   head "https://github.com/codedeviate/batty.git", branch: "main"
 
