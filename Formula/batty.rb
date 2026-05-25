f0e4100eca6a8bdc9bcd4c435c45f3160c19b55d
Thomas Björk <codedv8@gmail.com>
2026-05-25 22:05:42 +0200

    batty 0.13.1

--- Formula/batty.rb
+++ Formula/batty.rb
@@ -1,8 +1,8 @@
 class Batty < Formula
   desc "Cat clone with syntax highlighting, git integration, and Rhai support"
   homepage "https://github.com/codedeviate/batty"
-  url "https://github.com/codedeviate/batty/archive/refs/tags/v0.13.0.tar.gz"
-  sha256 "8f8e8a7ad3ae84dadd30b14a69787dfaa6cc707e516fe59db3548753da185656"
+  url "https://github.com/codedeviate/batty/archive/refs/tags/v0.13.1.tar.gz"
+  sha256 "4470c7f4c13fdce680b0b22bae0d5414dbc6ff6859ac28e25ce305af4ae38a27"
   license "MIT"
   head "https://github.com/codedeviate/batty.git", branch: "main"
 
