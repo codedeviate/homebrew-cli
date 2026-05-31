02c7e6879a0e18b773d0a3129b760968d1206d36
Thomas Björk <codedv8@gmail.com>
2026-05-31 14:40:38 +0200

    sercon 0.26.0

--- Formula/sercon.rb
+++ Formula/sercon.rb
@@ -1,8 +1,8 @@
 class Sercon < Formula
   desc "Embeddable TypeScript script engine CLI (pure Go, no Node)"
   homepage "https://github.com/codedeviate/sercon"
-  url "https://github.com/codedeviate/sercon/archive/refs/tags/v0.9.0.tar.gz"
-  sha256 "18ec70e649de0a3fc209c00f4843e696de91cb8755ea167d193f890e3f5f3685"
+  url "https://github.com/codedeviate/sercon/archive/refs/tags/v0.26.0.tar.gz"
+  sha256 "e6ce52250133400899f3289f40a7ddad4dd7c5b20fe3549f9cb975d681a0d098"
   license "MIT"
   head "https://github.com/codedeviate/sercon.git", branch: "master"
 
