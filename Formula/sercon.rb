0a8ae0b9e7976c1aa15c9a06e70f8b9dbef0440a
starweb-thomas <thomas.bjork@starweb.se>
2026-06-07 21:28:56 +0200

    sercon 0.39.0

--- Formula/sercon.rb
+++ Formula/sercon.rb
@@ -1,8 +1,8 @@
 class Sercon < Formula
   desc "Reconnaissance, shaped by code — TypeScript script engine"
   homepage "https://github.com/codedeviate/sercon"
-  url "https://github.com/codedeviate/sercon/archive/refs/tags/v0.35.1.tar.gz"
-  sha256 "11af061c86bb06cbafc40e2ec29781c22d39763fb5df907d168dc769e3f419db"
+  url "https://github.com/codedeviate/sercon/archive/refs/tags/v0.39.0.tar.gz"
+  sha256 "0ca53ed26ceb6e3875355cbc8b3c74ea498ee147e5a397e2ebf683dcd1b60d27"
   license "MIT"
   head "https://github.com/codedeviate/sercon.git", branch: "master"
 
