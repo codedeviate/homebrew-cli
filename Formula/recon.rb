d04c94491272d968ab6baa95503df9fd6acac710
Thomas Björk <codedv8@gmail.com>
2026-06-16 15:08:32 +0200

    recon 0.98.0

--- Formula/recon.rb
+++ Formula/recon.rb
@@ -1,8 +1,8 @@
 class Recon < Formula
   desc "Network reconnaissance CLI with HTTP/TLS/DNS probes and Rhai scripting"
   homepage "https://github.com/codedeviate/recon"
-  url "https://github.com/codedeviate/recon/archive/refs/tags/v0.96.1.tar.gz"
-  sha256 "a513dcb31021ac3a22a62346d78be7c78c1b6c00a1c963a298b89b229fd7d033"
+  url "https://github.com/codedeviate/recon/archive/refs/tags/v0.98.0.tar.gz"
+  sha256 "8dda54785e8e36756e3b75d6d2ab0cde5820369a5e60ad16fd982bc13d161ddd"
   license "MIT"
   head "https://github.com/codedeviate/recon.git", branch: "master"
 
