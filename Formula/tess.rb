fe1aefd47947023db88602c2abc9203c54a046b5
Thomas Björk <codedv8@gmail.com>
2026-06-17 16:16:52 +0200

    tess 0.39.0

--- Formula/tess.rb
+++ Formula/tess.rb
@@ -1,8 +1,8 @@
 class Tess < Formula
   desc "Less-style terminal pager with structured-log filtering and pretty-printing"
   homepage "https://github.com/codedeviate/tess"
-  url "https://github.com/codedeviate/tess/archive/refs/tags/v0.38.0.tar.gz"
-  sha256 "8744daa5054d37f22498ce2656bc2a3cda286e8f428a7ed716af20b84f32430f"
+  url "https://github.com/codedeviate/tess/archive/refs/tags/v0.39.0.tar.gz"
+  sha256 "56dbfc20ef4817271962c0c38f1fd68cbe8d291cbec7005d1157f49c6d846e4b"
   license "MIT"
   head "https://github.com/codedeviate/tess.git", branch: "master"
 
