f5776d3677dff217bf3ad87d578089554400bea4
Thomas Björk <codedv8@gmail.com>
2026-07-02 18:08:44 +0200

    sercon 0.84.0

--- Formula/sercon.rb
+++ Formula/sercon.rb
@@ -1,8 +1,8 @@
 class Sercon < Formula
   desc "Reconnaissance, shaped by code — TypeScript script engine"
   homepage "https://github.com/codedeviate/sercon"
-  url "https://github.com/codedeviate/sercon/archive/refs/tags/v0.83.0.tar.gz"
-  sha256 "7ca0874d5b3618a85ad62491abb6098766bc1b0ecbd5b9abf9932349d36cf8f3"
+  url "https://github.com/codedeviate/sercon/archive/refs/tags/v0.84.0.tar.gz"
+  sha256 "db323ba788f3486a6c00d27846ad51ffd125fbb65e81a7037d0367c62ea70f42"
   license "MIT"
   head "https://github.com/codedeviate/sercon.git", branch: "master"
 
